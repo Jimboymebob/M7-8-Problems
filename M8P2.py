f904f11ed1c100a50c909a7036f134e2576a403b
Jimboymebob <lx72945@mail.harpercollege.edu>
2026-04-13 19:28:50 -0500

    Add a simple incrementing loop in M8P2.py

---
+++ M8P2.py
@@ -0,0 +1,10 @@
+start = int(input("Enter start value: "))
+stop = int(input("Enter stop value: "))
+increment = int(input("Enter increment value: "))
+
+current = start
+
+while current <= stop:
+    print(current)
+    current += increment
+  
